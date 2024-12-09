3c051e93468bf4688dccd175d2d4eddc86de227d
OumaArera <johnouma999@gmail.com>
2024-12-09 11:31:14 +0300

    feat: refactor proxies

--- accountStructure/getAccount.js
+++ accountStructure/getAccount.js
@@ -6,32 +6,16 @@ require('dotenv').config();
 
 const router = express.Router();
 
-// Middleware to validate incoming data
-const validateUserId = (userId) => {
-  return Number.isInteger(userId);
-};
-
 router.get('/', authenticateToken, async (req, res) => {
 
   const userId = req.user.id; 
   if (!userId) {
     return res.status(401).json({
-      error: true,
       success: false,
       message: "Unauthorized: Missing user ID in token",
       statusCode: 401
     });
   };
-
-  // Validate userId
-  if (!validateUserId(userId)) {
-    return res.status(400).json({
-      error: true,
-      success: false,
-      message: 'Invalid userId',
-      statusCode: 400
-    });
-  }
 
   try {
     const account = await db.AccountStructure.findOne({
@@ -41,7 +25,6 @@ router.get('/', authenticateToken, async (req, res) => {
 
     if (!account) {
       return res.status(404).json({
-        error: true,
         success: false,
         message: 'Account structure not found',
         statusCode: 404
@@ -50,14 +33,12 @@ router.get('/', authenticateToken, async (req, res) => {
 
     return res.status(200).json({
       success: true,
-      error: false,
       data: account,
       statusCode: 200
     });
   } catch (error) {
     console.error('Error fetching account structure:', error);
     return res.status(500).json({
-      error: true,
       success: false,
       message: 'Internal server error',
       statusCode: 500
